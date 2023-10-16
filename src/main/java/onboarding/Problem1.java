e94f7cc10dc62f7474aaef5e7d4fabc5c3ac024a
jae woo Jung <awfjol2008@naver.com>
2023-10-16 20:50:48 +0900

    Problem 1~3 완료 (#2)
    * [Problem #1] feat: 기능 요구 사항 정리 및 기능 구현 완료
    
    * [Problem #2] docs: 기능 요구 사항 정리
    
    * [Problem #2] feat: 입력값의 길이를 검증하는 기능 구현
    
    * [Problem #2] feat: 입력값이 소문자로만 이루어져 있는지 검증하는 기능 구현
    
    * [Problem #2] feat: 입력값을 전체적으로 검증하는 기능 구현
    
    * [Problem #2] feat: 입력값에서 연속으로 중복하는 문자들을 제거하는 기능 구현
    
    * [Problem #2] feat: 입력값에 대한 검증 및 예외처리 후, 중복 문자를 제거한 결과를 반환하는 기능 구현
    
    * [Problem #2] feat: 최종 결과 반환 로직 적용
    
    * [Problem #3] docs: 기능 요구 사항 정리
    
    * [Problem #3] feat: 입력값 범위 검증 기능 구현
    
    * [Problem #3] docs: 기능 요구 사항 수정
    
    * [Problem #3] docs: 기능 요구 사항 수정
    
    * [Problem #3] feat: 입력값의 각 자리수를 추출하는 기능 구현
    
    * [Problem #3] feat: 추출된 각 자리수를 바탕으로 3, 6, 9의 개수를 구하는 기능 구현
    
    * [Problem #3] feat: 1부터 입력값까지의 각 숫자들로부터 반환되는 손뼉 수를 전부 더하는 기능 구현
    
    * [Problem #3] feat: 입력값 검증 및 예외처리 후 최종 손뼉 횟수를 반환하는 기능 구현
    
    * [Problem #3] feat: 최종 결과 반환 로직 적용

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,84 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        return getSolution(pobi, crong);
+    }
+
+    private static int getSolution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        // 1. 책을 임의로 펼친다. -> pobi, crong 파라미터를 통해 입력값 획득
+        // 입력값에 대한 검증 로직
+        try {
+            validatePage(pobi);
+            validatePage(crong);
+        } catch (IllegalArgumentException exception) {
+            System.out.println("ERROR -> " + exception.getMessage());
+            return -1;
+        }
+
+        // 2. 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+        // 3. 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+        // 4. 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
+        int pobiScore = getResultScore(pobi);
+        int crongScore = getResultScore(crong);
+
+        // 5. 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+        answer = getWinPlayer(pobiScore, crongScore);
+
         return answer;
+    }
+
+    private static int getWinPlayer(int score1, int score2) {
+        try {
+            if (score1 > score2) {
+                return 1;
+            } else if (score1 < score2) {
+                return 2;
+            } else {
+                return 0;
+            }
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    private static int getResultScore(List<Integer> player) {
+        int page1 = player.get(0);
+        int page2 = player.get(1);
+
+        int maxLeft = Math.max(multiplyEveryNum(page1), addEveryNum(page1));
+        int maxRight = Math.max(multiplyEveryNum(page2), addEveryNum(page2));
+
+        return Math.max(maxLeft, maxRight);
+    }
+
+    private static void validatePage(List<Integer> player) {
+        if (player.get(1) != player.get(0) + 1) {
+            throw new IllegalArgumentException("페이지의 순서가 알맞지 않습니다.");
+        }
+    }
+
+    private static int addEveryNum(int num) {
+        int num1 = num % 10;
+        int num10 = (num % 100) / 10;
+        int num100 = num / 100;
+
+        return num1 + num10 + num100;
+    }
+
+    private static int multiplyEveryNum(int num) {
+        int num1 = num % 10;
+        int num10 = (num % 100) / 10;
+        int num100 = num / 100;
+
+        if (num > 10 && num < 100) {
+            num100 = 1;
+        } else if (num < 10) {
+            num100 = 1;
+            num10 = 1;
+        }
+
+        return num1 * num10 * num100;
     }
 }
