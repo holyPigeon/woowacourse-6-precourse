c1a344c7178563ee3c914cebc31b771fb35a2950
jae woo Jung <awfjol2008@naver.com>
2023-10-17 14:31:56 +0900

    [Problem #6] 완료 (#5)
    * [Problem #5] docs: 기능 목록 정리
    
    * [Problem #5] feat: 입력값의 범위를 검증하는 기능 구현
    
    * [Problem #5] feat: 입력값의 각 자리수를 리스트에 저장하는 기능 구현
    
    * [Problem #5] docs: 기능 목록 수정
    
    * [Problem #5] feat: 입력값의 각 자리수를 얻는 기능 제거
    
    * [Problem #5] feat: 입력값을 큰 금액 순으로 나누고, 각 화폐의 개수를 리스트에 저장 후 반환하는 기능 구현
    
    * [Problem #5] feat: 입력값 검증 후, 로직에 따라 최종적인 결과 리스트를 반환하는 기능 구현
    
    * [Problem #5] feat: 최종 결과 반환 로직 반영
    
    * [Problem #6] docs: 기능 목록 정리
    
    * [Problem #6] feat: 크루가 몇 명인지 검증하는 기능 구현
    
    * [Problem #6] feat: 이메일의 길이를 검증하는 기능 구현
    
    * [Problem #6] feat: 이메일이 이메일의 형식을 갖추고 있는지 검증하는 기능 구현
    
    * [Problem #6] feat: 닉네임의 길이를 검증하는 기능 구현
    
    * [Problem #6] docs: 중복 이메일 판별 관련 기능 정리 수정
    
    * [Problem #6] feat: 이메일과 닉네임 입력값을 검증하는 기능 구현
    
    * [Problem #6] feat: 닉네임 사이에서 중복된 문자열을 판별하는 기능 구현
    
    * [Problem #6] feat: 중복 기준에 부합하는 이메일들을 리스트로 반환하는 기능 구현
    
    * [Problem #6] feat: 중복 기준에 부합하는 이메일들을 판별하여 리스트로 반환하는 기능 구현
    
    * [Problem #6] feat: 반환된 결과 리스트를 오름차순으로 정렬하는 기능 구현
    
    * [Problem #6] feat: 이메일과 닉네임 검증 후, 중복된 이메일만을 선택 및 정렬하여 최종 반환하는 기능 구현
    
    * [Problem #6] feat: 최종 결과 반환 로직 적용

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,96 @@
 package onboarding;
 
-import java.util.List;
+import java.util.*;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        return getSolution(forms);
+    }
+
+    public static List<String> getSolution(List<List<String>> forms) {
+        try {
+            validateForm(forms);
+        } catch (IllegalArgumentException exception) {
+            System.out.println("ERROR -> " + exception.getMessage());
+        }
+
+        return getAscSortedList(getDuplicateEmailList(forms));
+    }
+
+     public static List<String> getAscSortedList(List<String> list) {
+         Collections.sort(list);
+         return list;
+     }
+
+    public static List<String> getDuplicateEmailList(List<List<String>> forms) {
+        return findDuplicates(forms);
+    }
+
+    private static List<String> findDuplicates(List<List<String>> data) {
+        Map<String, String> substrings = new HashMap<>();
+        Set<String> duplicates = new HashSet<>();
+
+        for (List<String> entry : data){
+            processNickname(substrings, duplicates, entry.get(1), entry.get(0));
+        }
+
+        return new ArrayList<>(duplicates);
+    }
+
+    private static void processNickname(Map<String, String> substrings,
+                                        Set<String> duplicates,
+                                        String nickname,
+                                        String email){
+        int windowSize = 2;
+
+        for(int i=0; i<=nickname.length()-windowSize; i++){
+            String subStr=nickname.substring(i,i+windowSize);
+
+            if(substrings.containsKey(subStr)){
+                duplicates.add(email);
+                duplicates.add(substrings.get(subStr));
+            } else{
+                substrings.put(subStr, email);
+            }
+        }
+    }
+
+    public static void validateForm(List<List<String>> forms) {
+        validateCrewNumber(forms);
+        validateEmailLength(forms);
+        validateEmailForm(forms);
+        validateNicknameLength(forms);
+    }
+
+    public static void validateCrewNumber(List<List<String>> forms) {
+        int crewNumber = forms.size();
+
+        if (crewNumber < 1 || crewNumber > 10000) {
+            throw new IllegalArgumentException("크루는 1명 이상 10,000명 이하이어야 합니다.");
+        }
+    }
+
+    public static void validateEmailLength(List<List<String>> forms) {
+        for (List<String> form : forms) {
+            if (form.get(0).length() < 11 || form.get(0).length() > 10000) {
+                throw new IllegalArgumentException("이메일의 길이는 11자 이상 20자 미만이어야 합니다.");
+            }
+        }
+    }
+
+    public static void validateEmailForm(List<List<String>> forms) {
+        for (List<String> form : forms) {
+            if (!form.get(0).matches("^.+@email\\.com$")) {
+                throw new IllegalArgumentException("이메일은 이메일 형식에 부합해야 합니다.");
+            }
+        }
+    }
+
+    public static void validateNicknameLength(List<List<String>> forms) {
+        for (List<String> form : forms) {
+            if (form.get(1).length() < 1 || form.get(1).length() > 10000) {
+                throw new IllegalArgumentException("닉네임의 길이는 1자 이상 20자 미만이어야 합니다.");
+            }
+        }
     }
 }
