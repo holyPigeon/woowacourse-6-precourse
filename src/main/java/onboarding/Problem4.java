024026941ea60cbe50c4288517c8b42e6ca80ab3
jae woo Jung <awfjol2008@naver.com>
2023-10-17 10:32:46 +0900

    Problem 4 완료 (#3)
    * [Problem #1] feat: 기능 요구 사항 정리 및 기능 구현 완료
    
    * [Problem #2] docs: 기능 요구 사항 정리
    
    * [Problem #2] feat: 입력값의 길이를 검증하는 기능 구현
    
    * [Problem #2] feat: 입력값이 소문자로만 이루어져 있는지 검증하는 기능 구현
    
    * [Problem #2] feat: 입력값을 전체적으로 검증하는 기능 구현
    
    * [Problem #2] feat: 입력값에서 연속으로 중복하는 문자들을 제거하는 기능 구현
    
    * [Problem #2] feat: 입력값에 대한 검증 및 예외처리 후, 중복 문자를 제거한 결과를 반환하는 기능 구현
    
    * [Problem #2] feat: 최종 결과 반환 로직 적용
    
    * [Problem #3] docs: 기능 요구 사항 정리
    
    * [Problem #3] feat: 입력값 범위 검증 기능 구현
    
    * [Problem #3] docs: 기능 요구 사항 수정
    
    * [Problem #3] docs: 기능 요구 사항 수정
    
    * [Problem #3] feat: 입력값의 각 자리수를 추출하는 기능 구현
    
    * [Problem #3] feat: 추출된 각 자리수를 바탕으로 3, 6, 9의 개수를 구하는 기능 구현
    
    * [Problem #3] feat: 1부터 입력값까지의 각 숫자들로부터 반환되는 손뼉 수를 전부 더하는 기능 구현
    
    * [Problem #3] feat: 입력값 검증 및 예외처리 후 최종 손뼉 횟수를 반환하는 기능 구현
    
    * [Problem #3] feat: 최종 결과 반환 로직 적용
    
    * [Problem #4] docs: 기능 목록 정리
    
    * [Problem #4] feat: 입력 문자열의 길이를 검증하는 기능 구현
    
    * [Problem #4] feat: 입력 문자열의 각 문자가 알파벳인지 확인하는 기능 구현
    
    * [Problem #4] feat: 입력 문자열의 각 문자가 소문자인지 대문자인지 확인하는 기능 구현
    
    * [Problem #4] feat: 입력 문자열의 각 문자가 소문자인지 대문자인지 확인하는 기능 삭제
    
    * [Problem #4] feat: 입력 문자열의 각 문자가 알파벳인지 확인하는 기능 삭제
    
    * [Problem #4] feat: 청개구리 사전에 따라 변환한 결과 문자열을 반환하는 기능 구현
    
    * [Problem #4] feat: 최종적으로 입력 문자열을 변환한 결과 문자열을 반환하는 기능 구현
    
    * [Problem #4] feat: 최종 결과 반환 로직 적용

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,44 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return getSolution(word);
+    }
+
+    public static String getSolution(String string) {
+        try {
+            validateStringLength(string);
+        } catch (IllegalArgumentException exception) {
+            System.out.println("ERROR -> " + exception.getMessage());
+        }
+
+        return convertWord(string);
+    }
+
+    public static String convertWord(String string) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+
+            if (Character.isUpperCase(c)) { // 대문자인 경우만 변환
+                int diff = c - 'A';
+                char convertedChar = (char) ('Z' - diff);
+                sb.append(convertedChar);
+            } else if (Character.isLowerCase(c)) { // 소문자인 경우만 변환
+                int diff = c - 'a';
+                char convertedChar = (char) ('z' - diff);
+                sb.append(convertedChar);
+            } else { // 알파벳이 아닌 경우 그대로 추가
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void validateStringLength(String string) {
+        if (string.length() < 1 || string.length() > 1000) {
+            throw new IllegalArgumentException("입력 문자열의 길이는 1 이상 1,000 이하이어야 합니다.");
+        }
     }
 }
