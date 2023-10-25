bbc694d77cf6b27ed211717f749feda6287aa00f
jae woo Jung <awfjol2008@naver.com>
2023-10-25 20:25:41 +0900

    feat: 로또 구입 금액 입력값을 전체적으로 검증하는 기능 구현

--- src/main/java/lotto/domain/player/PurchaseAmount.java
+++ src/main/java/lotto/domain/player/PurchaseAmount.java
@@ -6,21 +6,27 @@ public class PurchaseAmount {
 
     private int purchaseAmount;
 
-    public void validateEmpty(String string) {
-        if (string.isEmpty()) {
+    public void validate(String purchaseAmount) {
+        validateEmpty(purchaseAmount);
+        validateDigit(purchaseAmount);
+        validateUnit(purchaseAmount);
+    }
+
+    public void validateEmpty(String purchaseAmount) {
+        if (purchaseAmount.isEmpty()) {
             throw new IllegalArgumentException("구입 금액은 비어있지 않아야 합니다.");
         }
     }
 
-    public void validateDigit(String string) {
-        if (string.chars()
+    public void validateDigit(String purchaseAmount) {
+        if (purchaseAmount.chars()
                 .anyMatch(ch -> !Character.isDigit(ch))) {
             throw new IllegalArgumentException("구입 금액은 정수이어야 합니다.");
         }
     }
 
-    public void validateUnit(String string) {
-        if (parseInt(string) % 1000 != 0) {
+    public void validateUnit(String purchaseAmount) {
+        if (parseInt(purchaseAmount) % 1000 != 0) {
             throw new IllegalArgumentException("구입 금액은 1,000원 단위이어야 합니다.");
         }
     }
