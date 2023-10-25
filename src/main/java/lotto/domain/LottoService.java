3a734e658a946ae9545542330ecfffe3e7ecb8fb
jae woo Jung <awfjol2008@naver.com>
2023-10-25 21:08:04 +0900

    feat: 로또를 발행하는 기능 구현

--- src/main/java/lotto/domain/LottoService.java
+++ src/main/java/lotto/domain/LottoService.java
@@ -5,6 +5,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class LottoService {
+
+    public Lotto generateLotto(List<Integer> randomNumberList) {
+        return new Lotto(randomNumberList);
+    }
+
     public List<Integer> generateRandomNumberList() {
         return Randoms.pickUniqueNumbersInRange(1, 45, 6);
     }
