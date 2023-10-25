9e6992774d8c0bf5978f1fbbf2d0fa94bf50d800
jae woo Jung <awfjol2008@naver.com>
2023-10-25 22:10:38 +0900

    feat: 보너스 번호 입력값이 빈 문자열이 아닌지 검증하는 기능 구현

--- src/main/java/lotto/domain/player/BonusNumber.java
+++ src/main/java/lotto/domain/player/BonusNumber.java
@@ -2,4 +2,10 @@ package lotto.domain.player;
 
 public class BonusNumber {
     private int bonusNumber;
+
+    public void validateEmpty(String bonusNumber) {
+        if (bonusNumber.isEmpty()) {
+            throw new IllegalArgumentException("보너스 숫자는 비어있지 않아야 합니다.");
+        }
+    }
 }
