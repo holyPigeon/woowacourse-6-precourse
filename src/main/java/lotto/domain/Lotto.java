7295b16f395d08b5c54fa27697017ee4f5334151
jae woo Jung <awfjol2008@naver.com>
2023-10-25 23:50:27 +0900

    feat: 당첨 번호 입력값을 ", " 문자열을 기준으로 나누는 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -16,6 +16,14 @@ public class Lotto {
         }
     }
 
+    public List<Integer> convertLottoStringToList(String lottoString) {
+        String[] splitResult = lottoString.split(", ");
+        validateSplit(lottoString, splitResult);
+        return Arrays.stream(splitResult)
+                .map(Integer::parseInt)
+                .toList();
+    }
+
     public void validateSplit(String originalString, String[] splitResult) {
         if (originalString.equals(splitResult[0])) {
             throw new IllegalArgumentException("당첨 번호는 \", \"를 기준으로 구분되어야 합니다.");
