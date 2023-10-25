3189654be2175be4d1a92b0f8d8c34877cbfb49e
jae woo Jung <awfjol2008@naver.com>
2023-10-25 20:10:15 +0900

    feat: 로또 구입 금액 입력값이 빈 문자열이 아닌지 검증하는 기능 구현

--- src/main/java/lotto/domain/player/PurchaseAmount.java
+++ src/main/java/lotto/domain/player/PurchaseAmount.java
@@ -3,4 +3,10 @@ package lotto.domain.player;
 public class PurchaseAmount {
 
     private int purchaseAmount;
+
+    public void validateEmpty(String string) {
+        if (string.isEmpty()) {
+            throw new IllegalArgumentException("구입 금액은 비어있지 않아야 합니다.");
+        }
+    }
 }
