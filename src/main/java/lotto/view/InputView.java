5b155732fbca29dbd55d8c2f12894b92c842518e
jae woo Jung <awfjol2008@naver.com>
2023-10-26 20:47:26 +0900

    feat: InputView 클래스 싱글톤 패턴 적용
    InputView 클래스 관련 인스턴스를 하나만 생성할 수 있도록 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,6 +4,16 @@ import static camp.nextstep.edu.missionutils.Console.*;
 
 public class InputView {
 
+    private static final InputView instance = new InputView();
+
+    private InputView() {
+
+    }
+
+    public static InputView getInstance() {
+        return instance;
+    }
+
      public String read() {
          return readLine();
      }
