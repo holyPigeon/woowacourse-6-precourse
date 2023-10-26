0a49e907b93221d90a424dcea1a92c47f1c2a975
jae woo Jung <awfjol2008@naver.com>
2023-10-26 14:59:18 +0900

    feat: Player 클래스 복권 구입 관련 멤버 변수 추가
    List<Lotto> lottoList: 구매한 로또 리스트
    PurchaseAmount purchaseAmount: 로또 구입 금액
    BonusNumber bonusNumber: 보너스 번호

--- src/main/java/lotto/domain/player/Player.java
+++ src/main/java/lotto/domain/player/Player.java
@@ -1,4 +1,13 @@
 package lotto.domain.player;
 
+import lotto.domain.Lotto;
+
+import java.util.List;
+
 public class Player {
+    private List<Lotto> lottoList;
+
+    private PurchaseAmount purchaseAmount;
+
+    private BonusNumber bonusNumber;
 }
