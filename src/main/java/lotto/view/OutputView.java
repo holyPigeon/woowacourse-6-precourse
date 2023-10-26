dc9bc9d5a758bcaf9d635f3c7a9a27a7a501ec4f
jae woo Jung <awfjol2008@naver.com>
2023-10-26 21:53:29 +0900

    feat: 발행된 로또의 개수를 출력하는 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -16,6 +16,10 @@ public class OutputView {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
+    public void printGeneratedLottoCountMessage(int count) {
+        System.out.println(8 + "개를 구매했습니다.");
+    }
+
     public void printLottoNumberMessage() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
