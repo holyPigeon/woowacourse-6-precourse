03a1c07429cafd772dd73969fd49329739899451
jae woo Jung <awfjol2008@naver.com>
2023-10-27 00:41:39 +0900

    feat: 로또 발행 결과를 출력하는 기능 구현

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,7 +1,14 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Integer.*;
+import static lotto.domain.Lotto.*;
 
 public class LottoController {
 
@@ -13,7 +20,17 @@ public class LottoController {
         return inputView.read();
     }
 
+    private void printGeneratedLottoStatus(String purchaseAmount) {
+        int lottoCount = getLottoCount(purchaseAmount);
+        List<Lotto> lottoList = new ArrayList<>();
+
+        for (int i = 0; i < lottoCount; i++) {
+            lottoList.add(new Lotto(generateWinningNumber()));
+            outputView.printGeneratedLottoNumberMessage(lottoList.get(i).toString());
+        }
+    }
+
     public int getLottoCount(String purchaseAmount) {
-        return Integer.parseInt(purchaseAmount) / 1000;
+        return parseInt(purchaseAmount) / 1000;
     }
 }
