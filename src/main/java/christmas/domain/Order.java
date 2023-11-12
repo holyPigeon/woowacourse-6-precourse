7ebb0979da9abde4c4cd29e76baf6d44fcf98ae0
jae woo Jung <awfjol2008@naver.com>
2023-11-12 22:45:56 +0900

    refector: Order 클래스의 멤버 변수 타입에 Quantity VO 적용

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -7,24 +7,24 @@ import java.util.Map;
 
 public class Order {
 
-    Map<Menu, Integer> customerMenus = new EnumMap<>(Menu.class);
+    Map<Menu, Quantity> customerMenus = new EnumMap<>(Menu.class);
 
-    public CustomerMenus(Map<Menu, Integer> customerMenus) {
+    public Order(Map<Menu, Quantity> customerMenus) {
         validate(customerMenus);
         this.customerMenus = customerMenus;
     }
 
-    private void validate(Map<Menu, Integer> customerMenus) {
+    private void validate(Map<Menu, Quantity> customerMenus) {
         validateHasDuplicateMenu(customerMenus);
     }
 
-    private void validateHasDuplicateMenu(Map<Menu, Integer> customerMenus) {
+    private void validateHasDuplicateMenu(Map<Menu, Quantity> customerMenus) {
         if (hasDuplicateMenu(customerMenus)) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 
-    private boolean hasDuplicateMenu(Map<Menu, Integer> customerMenus) {
+    private boolean hasDuplicateMenu(Map<Menu, Quantity> customerMenus) {
         return customerMenus.keySet()
                 .stream()
                 .distinct()
