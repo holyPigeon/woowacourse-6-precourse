fcae39dee6f0eeacba057b8d6c9973c38e3c2989
jae woo Jung <awfjol2008@naver.com>
2023-11-12 23:15:08 +0900

    feat: 날짜 입력값 정수의 범위가 1~31인지 검증하는 기능 구현

--- src/main/java/christmas/domain/Date.java
+++ src/main/java/christmas/domain/Date.java
@@ -5,6 +5,21 @@ public class Date {
     private Integer date;
 
     public Date(Integer date) {
+        validate(date);
         this.date = date;
+    }
+
+    private void validate(Integer date) {
+        validateIsDateInRange(date);
+    }
+
+    private void validateIsDateInRange(Integer date) {
+        if (isNumberInRange(date)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private boolean isNumberInRange(Integer number) {
+        return number < 1  || number > 31;
     }
 }
