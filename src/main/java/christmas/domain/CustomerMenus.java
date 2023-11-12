36c69148607f93a1597b2260542e03c5a55688cb
jae woo Jung <awfjol2008@naver.com>
2023-11-12 19:51:51 +0900

    feat: CustomerMenus 클래스 멤버변수 추가

--- src/main/java/christmas/domain/CustomerMenus.java
+++ src/main/java/christmas/domain/CustomerMenus.java
@@ -1,4 +1,11 @@
 package christmas.domain;
 
+import christmas.Menu;
+
+import java.util.EnumMap;
+import java.util.Map;
+
 public class CustomerMenus {
+
+    Map<Menu, Integer> customerMenus = new EnumMap<>(Menu.class);
 }
