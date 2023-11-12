787a8c7d5e895329ba47550dc1a1a386af2e04b9
jae woo Jung <awfjol2008@naver.com>
2023-11-12 22:42:59 +0900

    feat: 메뉴당 개수 입력값이 조건값 이상인지 검증하는 기능 구현

--- src/main/java/christmas/domain/Quantity.java
+++ src/main/java/christmas/domain/Quantity.java
@@ -5,6 +5,17 @@ public class Quantity {
     private Integer quantity;
 
     public Quantity(Integer quantity) {
+        validate(quantity);
         this.quantity = quantity;
+    }
+
+    private void validate(Integer quantity) {
+        validateIsGreaterThanCondition(quantity);
+    }
+
+    private void validateIsGreaterThanCondition(Integer quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
 }
