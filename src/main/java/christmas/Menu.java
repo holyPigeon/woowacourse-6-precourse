92f5a7f857f21fc5f3b4c88f16579fcc1e666a47
jae woo Jung <awfjol2008@naver.com>
2023-11-12 22:26:49 +0900

    feat: 입력된 메뉴 리스트에 중복된 메뉴가 없는지 검증하는 기능 구현

--- src/main/java/christmas/Menu.java
+++ src/main/java/christmas/Menu.java
@@ -4,15 +4,22 @@ import static christmas.MenuType.*;
 
 public enum Menu {
 
+    // 에피타이저
     MUSHROOM_SOUP("양송이 스프", 6_000, APPETIZER),
     TAPAS("타파스", 5_500, APPETIZER),
     CAESAR_SALAD("시저 샐러드", 8_000, APPETIZER),
+
+    // 메인
     T_BONE_STEAK("티본스테이크", 55_000, MAIN),
     BBQ_RIBS("바비큐립", 54_000, MAIN),
     SEAFOOD_PASTA("해산물파스타", 35_000, MAIN),
     CHRISTMAS_PASTA("크리스마스파스타", 25_000, MAIN),
+
+    // 디저트
     CHOCOLATE_CAKE("초코케이크", 15_000, DESSERT),
     ICE_CREAM("아이스크림", 5_000, DESSERT),
+
+    // 음료
     ZERO_COKE("제로콜라", 3_000, DRINK),
     RED_WINE("레드와인", 60_000, DRINK),
     CHAMPAGNE("샴페인", 25_000, DRINK);
--- src/main/java/christmas/domain/CustomerMenus.java
+++ src/main/java/christmas/domain/CustomerMenus.java
@@ -10,6 +10,24 @@ public class CustomerMenus {
     Map<Menu, Integer> customerMenus = new EnumMap<>(Menu.class);
 
     public CustomerMenus(Map<Menu, Integer> customerMenus) {
+        validate(customerMenus);
         this.customerMenus = customerMenus;
+    }
+
+    private void validate(Map<Menu, Integer> customerMenus) {
+        validateHasDuplicateMenu(customerMenus);
+    }
+
+    private void validateHasDuplicateMenu(Map<Menu, Integer> customerMenus) {
+        if (hasDuplicateMenu(customerMenus)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private boolean hasDuplicateMenu(Map<Menu, Integer> customerMenus) {
+        return customerMenus.keySet()
+                .stream()
+                .distinct()
+                .count() != customerMenus.size();
     }
 }
