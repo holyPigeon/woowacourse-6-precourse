f38fbb63c80d8f3e4dcc6e62690ec7809fab8ef2
jae woo Jung <awfjol2008@naver.com>
2023-11-12 23:48:52 +0900

    refector: Day 클래스명 변경사항 적용

--- src/main/java/christmas/domain/Day.java
+++ src/main/java/christmas/domain/Day.java
@@ -2,19 +2,19 @@ package christmas.domain;
 
 public class Day {
 
-    private Integer date;
+    private Integer day;
 
-    public Day(Integer date) {
-        validate(date);
-        this.date = date;
+    public Day(Integer day) {
+        validate(day);
+        this.day = day;
     }
 
-    private void validate(Integer date) {
-        validateIsDateInRange(date);
+    private void validate(Integer day) {
+        validateIsDateInRange(day);
     }
 
-    private void validateIsDateInRange(Integer date) {
-        if (isNumberInRange(date)) {
+    private void validateIsDateInRange(Integer day) {
+        if (isNumberInRange(day)) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
@@ -23,7 +23,7 @@ public class Day {
         return number < 1  || number > 31;
     }
 
-    public Integer getDate() {
-        return date;
+    public Integer getDay() {
+        return day;
     }
 }
--- src/main/java/christmas/domain/discount/DdayDiscount.java
+++ src/main/java/christmas/domain/discount/DdayDiscount.java
@@ -10,13 +10,13 @@ public class DdayDiscount implements Discount{
     @Override
     public int calculateDiscountAmount(Map<Menu, Quantity> customerMenus, Day day) {
         if (isAvailableDiscount(customerMenus, day)) {
-            return 1000 + 100 * (day.getDate() - 1);
+            return 1000 + 100 * (day.getDay() - 1);
         }
         return 0;
     }
 
     @Override
     public boolean isAvailableDiscount(Map<Menu, Quantity> customerMenus, Day day) {
-        return day.getDate() >= 1 && day.getDate() <= 25;
+        return day.getDay() >= 1 && day.getDay() <= 25;
     }
 }
