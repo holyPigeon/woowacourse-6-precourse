39d8c556f998a75622e17b4aae78ba94534a1583
jae woo Jung <awfjol2008@naver.com>
2023-11-13 01:18:34 +0900

    feat: Quantity 클래스 getter 추가

--- src/main/java/christmas/domain/Quantity.java
+++ src/main/java/christmas/domain/Quantity.java
@@ -18,4 +18,8 @@ public class Quantity {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
+
+    public Integer getPrimitiveQuantity() {
+        return quantity;
+    }
 }
