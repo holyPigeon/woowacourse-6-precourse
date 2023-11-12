b72745dcb5da97f212068632e2055e9d5cbaaba5
jae woo Jung <awfjol2008@naver.com>
2023-11-13 00:00:43 +0900

    feat: 평일 할인 클래스 구현

---
+++ src/main/java/christmas/domain/discount/WeekdayDiscount.java
@@ -0,0 +1,37 @@
+package christmas.domain.discount;
+
+import christmas.Menu;
+import christmas.MenuType;
+import christmas.domain.Day;
+import christmas.domain.Quantity;
+import christmas.domain.util.DateUtil;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+public class WeekdayDiscount implements Discount{
+    @Override
+    public int calculateDiscountAmount(Map<Menu, Quantity> customerMenus, Day day) {
+        if (isWeekday(day)) {
+            return findDiscountableMenuCount(customerMenus) * 2023;
+        }
+        return 0;
+    }
+
+    private static int findDiscountableMenuCount(Map<Menu, Quantity> customerMenus) {
+        return (int) customerMenus.keySet()
+                .stream()
+                .filter(menu -> menu.getType().equals(MenuType.DESSERT))
+                .count();
+    }
+
+    @Override
+    private boolean isAvailableDiscount(Map<Menu, Quantity> customerMenus, Day day) {
+        return isWeekday(day);
+    }
+
+    private boolean isWeekday(Day day) {
+        LocalDate localDate = DateUtil.convertDayToLocalDate(day);
+        return localDate.getDayOfWeek().getValue() <= 5; // 1: Monday, 2: Tuesday, ..., 7: Sunday
+    }
+}
