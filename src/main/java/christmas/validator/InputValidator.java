c57b6e095d908caba0e5c244ed595c3ba075cdcb
jae woo Jung <awfjol2008@naver.com>
2023-11-14 03:46:01 +0900

    feat: 메뉴 및 개수 입력값이 올바른 포맷인지 검증하는 기능 구현

--- src/main/java/christmas/validator/InputValidator.java
+++ src/main/java/christmas/validator/InputValidator.java
@@ -1,5 +1,7 @@
 package christmas.validator;
 
+import java.util.regex.Pattern;
+
 import static java.lang.Integer.parseInt;
 
 public class InputValidator {
@@ -8,15 +10,12 @@ public class InputValidator {
 
     }
 
+    /*
+    식당 예상 방문 날짜 입력값 검증
+     */
     public static void validateEstimatedVisitingDateInput(String input) {
         validateIsBlank(input);
         validateIsDigit(input);
-    }
-
-    private static void validateIsBlank(String input) {
-        if (input.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 입력값은 비어있지 않아야 합니다.");
-        }
     }
 
     private static void validateIsDigit(String input) {
@@ -31,6 +30,25 @@ public class InputValidator {
             return true;
         } catch (NumberFormatException e) {
             return false;
+        }
+    }
+
+    private static void validateIsRightFormat(String input) {
+        if (!isRightFormat(input)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static boolean isRightFormat(String input) {
+        String regex = "^([가-힣A-Za-z0-9]+-\\d+)(,[가-힣A-Za-z0-9]+-\\d+)*$"; // 티본스테이크-1,바비큐립-1
+        return Pattern.compile(regex)
+                .matcher(input)
+                .matches();
+    }
+
+    private static void validateIsBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 입력값은 비어있지 않아야 합니다.");
         }
     }
 }
