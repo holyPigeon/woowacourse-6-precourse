e8bb431d736b9adad37421d954bc744142890694
jae woo Jung <awfjol2008@naver.com>
2023-11-14 04:00:23 +0900

    feat: 정해진 포맷에 따라 메뉴 이름과 주문한 갯수를 분리하는 기능 구현

--- src/main/java/christmas/parser/InputParser.java
+++ src/main/java/christmas/parser/InputParser.java
@@ -1,8 +1,29 @@
 package christmas.parser;
 
+import christmas.dto.request.CustomerMenuRequest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class InputParser {
 
     private InputParser() {
 
+    }
+
+    public static List<CustomerMenuRequest> parseCustomerMenus(String input) {
+        return getEachNameAndQuantity(input)
+                .map(parts -> CustomerMenuRequest.of(parts[0], Integer.parseInt(parts[1])))
+                .toList();
+    }
+
+    private static Stream<String[]> getEachNameAndQuantity(String input) {
+        return getEachMenu(input)
+                .map(item -> item.split("-"));
+    }
+
+    private static Stream<String> getEachMenu(String input) {
+        return Arrays.stream(input.split(","));
     }
 }
