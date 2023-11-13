bb8008e8d72f220f302719655fcaf2b3e7c4085c
jae woo Jung <awfjol2008@naver.com>
2023-11-14 03:38:11 +0900

    feat: 주문 메뉴 및 개수 입력 메시지를 출력하는 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -16,4 +16,8 @@ public class InputView {
 
         return input;
     }
+
+    public String readCustomerMenus() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+    }
 }
