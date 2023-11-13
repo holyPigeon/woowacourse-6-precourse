578f49f5807fef6b61b98e15edd1edb31e02ad54
jae woo Jung <awfjol2008@naver.com>
2023-11-14 03:33:10 +0900

    feat: 입력값이 정수인지 검증하는 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,8 +1,19 @@
 package christmas.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import christmas.validator.InputValidator;
+
 public class InputView {
 
     private InputView() {
 
+    }
+
+    public String readEstimatedVisitingDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String input = Console.readLine();
+        InputValidator.validateIsBlank(input);
+
+        return input;
     }
 }
