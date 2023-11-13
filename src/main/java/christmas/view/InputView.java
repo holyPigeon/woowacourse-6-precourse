248bd25f97fa95b1f7f52cdaef68c857176a0c19
jae woo Jung <awfjol2008@naver.com>
2023-11-14 04:29:08 +0900

    feat: 주문 메뉴와 개수를 입력받는 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,7 +1,11 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.dto.request.CustomerMenuRequest;
+import christmas.parser.InputParser;
 import christmas.validator.InputValidator;
+
+import java.util.List;
 
 public class InputView {
 
@@ -17,8 +21,11 @@ public class InputView {
         return input;
     }
 
-    public String readCustomerMenus() {
+    public List<CustomerMenuRequest> readCustomerMenus() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
+        InputValidator.validateCustomerMenus(input);
+
+        return InputParser.parseCustomerMenus(input);
     }
 }
