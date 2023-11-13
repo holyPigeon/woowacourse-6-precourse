199b44b303baf63e21284635ee159fd3a4227363
jae woo Jung <awfjol2008@naver.com>
2023-11-14 03:36:41 +0900

    feat: 날짜 입력 메시지 출력 및 입력값을 입력받고 검증하는 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -12,7 +12,7 @@ public class InputView {
     public String readEstimatedVisitingDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
-        InputValidator.validateIsBlank(input);
+        InputValidator.validateEstimatedVisitingDateInput(input);
 
         return input;
     }
