4dc7780399f2588b69dabf38c2043add3ce1aa7b
jae woo Jung <awfjol2008@naver.com>
2023-11-14 03:35:33 +0900

    feat: 날짜 입력값을 전체적으로 검증하는 기능 구현

--- src/main/java/christmas/validator/InputValidator.java
+++ src/main/java/christmas/validator/InputValidator.java
@@ -1,14 +1,36 @@
 package christmas.validator;
 
+import static java.lang.Integer.parseInt;
+
 public class InputValidator {
 
     private InputValidator() {
 
     }
 
-    public static void validateIsBlank(String input) {
+    public static void validateEstimatedVisitingDateInput(String input) {
+        validateIsBlank(input);
+        validateIsDigit(input);
+    }
+
+    private static void validateIsBlank(String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 입력값은 비어있지 않아야 합니다.");
+        }
+    }
+
+    private static void validateIsDigit(String input) {
+        if (!isDigit(input)) {
+            throw new IllegalArgumentException("[ERROR] 입력값은 정수이어야 합니다.");
+        }
+    }
+
+    private static boolean isDigit(String input) {
+        try {
+            parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 }
