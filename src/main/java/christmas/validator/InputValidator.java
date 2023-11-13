c65ee3fc50b6d34173bfbd68b5a4925442cfff65
jae woo Jung <awfjol2008@naver.com>
2023-11-14 03:30:33 +0900

    feat: 입력값이 비어있지 않은지 검증하는 기능 구현

--- src/main/java/christmas/validator/InputValidator.java
+++ src/main/java/christmas/validator/InputValidator.java
@@ -5,4 +5,10 @@ public class InputValidator {
     private InputValidator() {
 
     }
+
+    public static void validateIsBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 입력값은 비어있지 않아야 합니다.");
+        }
+    }
 }
