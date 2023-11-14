931e0493f4582f7887990e26f68cba4fce5d105e
jae woo Jung <awfjol2008@naver.com>
2023-11-14 17:16:52 +0900

    feat: 주문 메뉴와 개수를 출력하는 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,8 +1,25 @@
 package christmas.view;
 
+import christmas.dto.response.OrderedMenuResponse;
+
+import java.util.List;
+
 public class OutputView {
 
     private OutputView() {
 
+    }
+
+    public static void printIntroductionMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    }
+
+    public static void printPreviewIntroductionMessage() {
+        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", 26);
+    }
+
+    public static void printOrderedMenusMessage(List<OrderedMenuResponse> orderedMenus) {
+        System.out.println();
+        orderedMenus.forEach(orderedMenu -> System.out.printf("%s %d개", orderedMenu.getName(), orderedMenu.getQuantity()));
     }
 }
