06f85681d24108ff7c2d3b2f6fce5512d9e5cc45
jae woo Jung <awfjol2008@naver.com>
2023-11-14 17:26:47 +0900

    feat: InputView 메소드 정적 메소드로 변경

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -13,7 +13,7 @@ public class InputView {
 
     }
 
-    public String readEstimatedVisitingDate() {
+    public static String readEstimatedVisitingDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
         InputValidator.validateEstimatedVisitingDateInput(input);
@@ -21,7 +21,7 @@ public class InputView {
         return input;
     }
 
-    public List<CustomerMenuRequest> readCustomerMenus() {
+    public static List<CustomerMenuRequest> readCustomerMenus() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
         InputValidator.validateCustomerMenus(input);
