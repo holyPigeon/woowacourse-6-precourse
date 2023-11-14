f9666c2675f83c9bd4a961637f78ede346440970
jae woo Jung <awfjol2008@naver.com>
2023-11-15 00:12:15 +0900

    feat: 혜택 미리보기 response dto 적용

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,6 +2,7 @@ package christmas.view;
 
 import christmas.domain.order.Badge;
 import christmas.dto.response.AvailableDiscountResponse;
+import christmas.dto.response.DiscountPreviewResponse;
 import christmas.dto.response.GiftMenuResponse;
 import christmas.dto.response.OrderedMenuResponse;
 
@@ -18,24 +19,35 @@ public class OutputView {
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
 
-    public static void printPreviewIntroductionMessage() {
+    public static void printDiscountPreviewMessage(DiscountPreviewResponse discountPreviewResponse) {
+        printDiscountPreviewIntroductionMessage();
+        printOrderedMenusMessage(discountPreviewResponse.getOrderedMenuResponses());
+        printInitialPriceMessage(discountPreviewResponse.getInitialPrice());
+        printGiftMenusMessage(discountPreviewResponse.getGiftMenuResponses());
+        printAvailableDiscountsMessage(discountPreviewResponse.getAvailableDiscountResponses());
+        printDiscountAmountMessage(discountPreviewResponse.getDiscountAmount());
+        printDiscountedPriceMessage(discountPreviewResponse.getDiscountedPrice());
+        printBadgeMessage(discountPreviewResponse.getBadge());
+    }
+
+    public static void printDiscountPreviewIntroductionMessage() {
         System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!%n", 26);
     }
 
-    public static void printOrderedMenusMessage(List<OrderedMenuResponse> orderedMenus) {
+    private static void printOrderedMenusMessage(List<OrderedMenuResponse> orderedMenus) {
         System.out.println();
         System.out.println("<주문 메뉴>");
         orderedMenus.forEach(orderedMenu -> System.out.printf("%s %d개%n", orderedMenu.getName(), orderedMenu.getQuantity()));
     }
 
-    public static void printInitialPriceMessage(int initialPrice) {
+    private static void printInitialPriceMessage(int initialPrice) {
         System.out.println();
         System.out.println("<할인 전 총주문 금액>");
         DecimalFormat decimalFormat = new DecimalFormat("###,###");
         System.out.printf("%s원%n", decimalFormat.format(initialPrice));
     }
 
-    public static void printGiftMenusMessage(List<GiftMenuResponse> giftMenus) {
+    private static void printGiftMenusMessage(List<GiftMenuResponse> giftMenus) {
         System.out.println();
         System.out.println("<증정 메뉴>");
         if (giftMenus.size() == 0) {
@@ -44,7 +56,7 @@ public class OutputView {
         giftMenus.forEach(giftMenu -> System.out.printf("%s %d개%n", giftMenu.getName(), giftMenu.getQuantity()));
     }
 
-    public static void printAvailableDiscountsMessage(List<AvailableDiscountResponse> availableDiscounts) {
+    private static void printAvailableDiscountsMessage(List<AvailableDiscountResponse> availableDiscounts) {
         System.out.println();
         System.out.println("<혜택 내역>");
         DecimalFormat decimalFormat = new DecimalFormat("###,###");
@@ -54,7 +66,7 @@ public class OutputView {
         availableDiscounts.forEach(availableDiscount -> System.out.printf("%s: -%s%n", availableDiscount.getName(), decimalFormat.format(availableDiscount.getDiscountAmount())));
     }
 
-    public static void printDiscountAmountMessage(int discountAmount) {
+    private static void printDiscountAmountMessage(int discountAmount) {
         System.out.println();
         System.out.println("<총혜택 금액>");
         DecimalFormat decimalFormat = new DecimalFormat("###,###");
@@ -67,14 +79,14 @@ public class OutputView {
 
     }
 
-    public static void printDiscountedPriceMessage(int discountedPrice) {
+    private static void printDiscountedPriceMessage(int discountedPrice) {
         System.out.println();
         System.out.println("<할인 후 예상 결제 금액>");
         DecimalFormat decimalFormat = new DecimalFormat("###,###");
         System.out.printf("%s원%n", decimalFormat.format(discountedPrice));
     }
 
-    public static void printBadgeMessage(Badge badge) {
+    private static void printBadgeMessage(Badge badge) {
         System.out.println();
         System.out.println("<12월 이벤트 배지>");
         System.out.print(badge.getName());
