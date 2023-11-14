82d5ef41e4c0d635833643b5cc7e3d976bce3f6d
jae woo Jung <awfjol2008@naver.com>
2023-11-15 00:13:58 +0900

    feat: PreviewController 구현

---
+++ src/main/java/christmas/controller/PreviewController.java
@@ -0,0 +1,58 @@
+package christmas.controller;
+
+import christmas.domain.PreviewService;
+import christmas.domain.order.Day;
+import christmas.domain.order.Order;
+import christmas.dto.response.DiscountPreviewResponse;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+import java.util.function.Supplier;
+
+public class PreviewController {
+
+    private PreviewController() {
+
+    }
+
+    public static PreviewController create() {
+        return new PreviewController();
+    }
+
+
+    public void run() {
+        printIntroductionMessage();
+
+        Day day = readWithExceptionHandling(PreviewController::readDay); // 날짜 입력
+        Order order = readWithExceptionHandling(PreviewController::readOrder); // 메뉴 입력
+        PreviewService previewService = PreviewService.create(order, day);
+
+        printDiscountPreviewMessage(order, previewService);
+    }
+
+    private static void printIntroductionMessage() {
+        OutputView.printIntroductionMessage(); // 인트로 메시지 출력
+    }
+
+    private static void printDiscountPreviewMessage(Order order, PreviewService previewService) {
+        OutputView.printDiscountPreviewMessage(DiscountPreviewResponse.of(previewService, order));
+    }
+
+    private static Day readDay() throws IllegalArgumentException {
+        return Day.create(InputView.readEstimatedVisitingDate());
+    }
+
+    private static Order readOrder() throws IllegalArgumentException {
+        return Order.create(InputView.readCustomerMenus());
+    }
+
+    private static <T> T readWithExceptionHandling(Supplier<T> reader) {
+        while (true) {
+            try {
+                return reader.get();
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+}
