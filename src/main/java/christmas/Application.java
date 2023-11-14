470339f5835612bfa7ed09738905ef546338ece6
jae woo Jung <awfjol2008@naver.com>
2023-11-15 00:14:24 +0900

    feat: 어플리케이션 실행 로직 적용

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,10 @@
 package christmas;
 
+import christmas.controller.PreviewController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PreviewController previewController = PreviewController.create();
+        previewController.run();
     }
 }
