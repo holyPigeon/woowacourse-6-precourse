0fb868cfd32c30e44d861f3ac6585573575ce032
jae woo Jung <awfjol2008@naver.com>
2023-11-14 23:27:38 +0900

    feat: OutputView 출력 포맷 전체적 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -19,45 +19,64 @@ public class OutputView {
     }
 
     public static void printPreviewIntroductionMessage() {
-        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!", 26);
+        System.out.printf("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!%n", 26);
     }
 
     public static void printOrderedMenusMessage(List<OrderedMenuResponse> orderedMenus) {
         System.out.println();
-        orderedMenus.forEach(orderedMenu -> System.out.printf("%s %d개", orderedMenu.getName(), orderedMenu.getQuantity()));
+        System.out.println("<주문 메뉴>");
+        orderedMenus.forEach(orderedMenu -> System.out.printf("%s %d개%n", orderedMenu.getName(), orderedMenu.getQuantity()));
     }
 
     public static void printInitialPriceMessage(int initialPrice) {
         System.out.println();
+        System.out.println("<할인 전 총주문 금액>");
         DecimalFormat decimalFormat = new DecimalFormat("###,###");
-        System.out.printf("%s원", decimalFormat.format(initialPrice));
+        System.out.printf("%s원%n", decimalFormat.format(initialPrice));
     }
 
     public static void printGiftMenusMessage(List<GiftMenuResponse> giftMenus) {
         System.out.println();
-        giftMenus.forEach(giftMenu -> System.out.printf("%s %d개", giftMenu.getName(), giftMenu.getQuantity()));
+        System.out.println("<증정 메뉴>");
+        if (giftMenus.size() == 0) {
+            System.out.println("없음");
+        }
+        giftMenus.forEach(giftMenu -> System.out.printf("%s %d개%n", giftMenu.getName(), giftMenu.getQuantity()));
     }
 
     public static void printAvailableDiscountsMessage(List<AvailableDiscountResponse> availableDiscounts) {
         System.out.println();
+        System.out.println("<혜택 내역>");
         DecimalFormat decimalFormat = new DecimalFormat("###,###");
-        availableDiscounts.forEach(availableDiscount -> System.out.printf("%s: -%s", availableDiscount.getName(), decimalFormat.format(availableDiscount.getDiscountAmount())));
+        if (availableDiscounts.size() == 0) {
+            System.out.println("없음");
+        }
+        availableDiscounts.forEach(availableDiscount -> System.out.printf("%s: -%s%n", availableDiscount.getName(), decimalFormat.format(availableDiscount.getDiscountAmount())));
     }
 
     public static void printDiscountAmountMessage(int discountAmount) {
         System.out.println();
+        System.out.println("<총혜택 금액>");
         DecimalFormat decimalFormat = new DecimalFormat("###,###");
-        System.out.printf("-%s원", decimalFormat.format(discountAmount));
+        if (discountAmount != 0) {
+            System.out.printf("-%s원%n", decimalFormat.format(discountAmount));
+        }
+        if (discountAmount == 0) {
+            System.out.printf("%s원%n", decimalFormat.format(discountAmount));
+        }
+
     }
 
     public static void printDiscountedPriceMessage(int discountedPrice) {
         System.out.println();
+        System.out.println("<할인 후 예상 결제 금액>");
         DecimalFormat decimalFormat = new DecimalFormat("###,###");
-        System.out.printf("%s원", decimalFormat.format(discountedPrice));
+        System.out.printf("%s원%n", decimalFormat.format(discountedPrice));
     }
 
     public static void printBadgeMessage(Badge badge) {
         System.out.println();
+        System.out.println("<12월 이벤트 배지>");
         System.out.print(badge.getName());
     }
 }
