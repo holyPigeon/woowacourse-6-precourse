2b9388adf5b63fadec21a4d7c80b6fa86e525980
jae woo Jung <awfjol2008@naver.com>
2023-11-14 23:45:34 +0900

    feat: 혜택 미리보기 response dto 구현

---
+++ src/main/java/christmas/dto/response/DiscountPreviewResponse.java
@@ -0,0 +1,60 @@
+package christmas.dto.response;
+
+import christmas.domain.PreviewService;
+import christmas.domain.order.Badge;
+import christmas.domain.order.Order;
+
+import java.util.List;
+
+public class DiscountPreviewResponse {
+
+    private List<OrderedMenuResponse> orderedMenuResponses;
+    private int initialPrice;
+    private List<GiftMenuResponse> giftMenuResponses;
+    private List<AvailableDiscountResponse> availableDiscountResponses;
+    private int discountAmount;
+    private int discountedPrice;
+    private Badge badge;
+
+    public DiscountPreviewResponse(PreviewService previewService, Order order) {
+        this.orderedMenuResponses = previewService.findOrderedMenus(order);
+        this.initialPrice = previewService.calculateInitialPrice(order);
+        this.giftMenuResponses = previewService.findGiftMenuResponses();
+        this.availableDiscountResponses = previewService.findAvailableDiscountResponses();
+        this.discountAmount = previewService.calculateDiscountAmount();
+        this.discountedPrice = previewService.calculateDiscountedPrice(order);
+        this.badge = previewService.calculateBadge(order);
+    }
+
+    public static DiscountPreviewResponse of(PreviewService previewService, Order order) {
+        return new DiscountPreviewResponse(previewService, order);
+    }
+
+    public List<OrderedMenuResponse> getOrderedMenuResponses() {
+        return orderedMenuResponses;
+    }
+
+    public int getInitialPrice() {
+        return initialPrice;
+    }
+
+    public List<GiftMenuResponse> getGiftMenuResponses() {
+        return giftMenuResponses;
+    }
+
+    public List<AvailableDiscountResponse> getAvailableDiscountResponses() {
+        return availableDiscountResponses;
+    }
+
+    public int getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public int getDiscountedPrice() {
+        return discountedPrice;
+    }
+
+    public Badge getBadge() {
+        return badge;
+    }
+}
