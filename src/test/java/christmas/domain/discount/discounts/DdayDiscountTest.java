cc1090d2511c590de46b6a205966517615b47f55
jae woo Jung <awfjol2008@naver.com>
2023-11-15 19:30:06 +0900

    test: 주문과 날짜 정보가 주어졌을 때, 할인 가능 여부를 설정하는 기능 테스트 작성

--- src/test/java/christmas/domain/discount/discounts/DdayDiscountTest.java
+++ src/test/java/christmas/domain/discount/discounts/DdayDiscountTest.java
@@ -1,19 +1,16 @@
 package christmas.domain.discount.discounts;
 
-import christmas.domain.discount.DiscountManager;
 import christmas.domain.order.Day;
 import christmas.domain.order.Order;
 import christmas.dto.request.OrderRequest;
 import christmas.util.InputUtil;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class DdayDiscountTest {
 
@@ -40,5 +37,15 @@ class DdayDiscountTest {
 
         // then
         assertThat(ddayDiscount.getDiscountAmount()).isEqualTo(1200);
+    }
+
+    @Test
+    @DisplayName("주문과 날짜 정보가 주어졌을 때, 할인 가능 여부를 설정한다.")
+    void return_is_discount_available_when_order_and_day_is_given() {
+        // when
+        ddayDiscount.checkIsAvailableDiscount(order, day);
+
+        // then
+        assertThat(ddayDiscount.getIsAvailable()).isEqualTo(true);
     }
 }
