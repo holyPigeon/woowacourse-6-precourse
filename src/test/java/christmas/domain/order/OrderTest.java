eef725c982be8af960de58af2a988d2425048515
jae woo Jung <awfjol2008@naver.com>
2023-11-15 18:57:22 +0900

    test: 주문 테스트 실행 전 초기화 진행

--- src/test/java/christmas/domain/order/OrderTest.java
+++ src/test/java/christmas/domain/order/OrderTest.java
@@ -1,5 +1,21 @@
 package christmas.domain.order;
 
+import christmas.dto.request.OrderRequest;
+import christmas.util.InputUtil;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.List;
+
 class OrderTest {
 
+    List<OrderRequest> orderRequests;
+    Order order;
+
+    @BeforeEach
+    void setUp() {
+        //given
+        String orderInput = "해산물파스타-2,아이스크림-2";
+        orderRequests = InputUtil.parseOrder(orderInput);
+        order = Order.create(orderRequests);
+    }
 }
