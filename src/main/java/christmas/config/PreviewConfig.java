109812f61e9e773e3afbda98d6f4037b9444690f
jae woo Jung <awfjol2008@naver.com>
2023-11-15 13:00:56 +0900

    feat: 전체 설정 관련 Enum 구현

---
+++ src/main/java/christmas/config/PreviewConfig.java
@@ -0,0 +1,33 @@
+package christmas.config;
+
+public enum PreviewConfig {
+
+    MONTH(12),
+    MINIMUM_DAY(1),
+    MAXIMUM_DAY(31),
+    TOTAL_QUANTITY_MAXIMUM(20),
+    EACH_QUANTITY_MINIMUM(1),
+    ;
+
+    private final int number;
+
+    PreviewConfig(int number) {
+        this.number = number;
+    }
+
+    public static boolean isDayInRange(Integer day) {
+        return day > MINIMUM_DAY.number  || day < MAXIMUM_DAY.number;
+    }
+
+    public static boolean isTotalQuantityGreaterThanCondition(int totalQuantity) {
+        return totalQuantity > TOTAL_QUANTITY_MAXIMUM.number;
+    }
+
+    public static boolean isEachQuantityValid(int eachQuantity) {
+        return eachQuantity < EACH_QUANTITY_MINIMUM.number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
