b0cdc2c600808e39fa0404871906a5374867f91a
jae woo Jung <awfjol2008@naver.com>
2023-11-15 17:34:49 +0900

    test: 이름을 통한 메뉴 검색 기능 테스트 작성

--- src/test/java/christmas/domain/order/menu/MenuTest.java
+++ src/test/java/christmas/domain/order/menu/MenuTest.java
@@ -1,8 +1,23 @@
 package christmas.domain.order.menu;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("메뉴 테스트")
 class MenuTest {
 
+    @Test
+    @DisplayName("메뉴 이름이 주어졌을 때, 해당 이름의 Menu를 반환한다.")
+    void return_Menu_when_menuName_is_given() {
+        //given
+        String menuName = "티본스테이크";
+
+        //when
+        Menu findMenu = Menu.findByName(menuName);
+
+        //then
+        assertThat(menuName).isEqualTo(findMenu.getName());
+    }
 }
