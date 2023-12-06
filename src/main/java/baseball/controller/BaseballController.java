19a0e00a9b303715482fad58357c550582c32044
jae woo Jung <awfjol2008@naver.com>
2023-12-06 11:06:00 +0900

    feat: 숫자 매칭 기능 구현

---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,45 @@
+package baseball.controller;
+
+import baseball.domain.Hint;
+import baseball.domain.Numbers;
+import baseball.domain.RandomNumberGenerator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class BaseballController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    private BaseballController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public static BaseballController create(InputView inputView, OutputView outputView) {
+        return new BaseballController(inputView, outputView);
+    }
+
+    public void run() {
+        outputView.gameStartMessage();
+        boolean gameRetry = false;
+
+        do {
+            Numbers computerNumbers = Numbers.create(RandomNumberGenerator.generateRandomNumbers());
+            matchNumbers(computerNumbers);
+        } while (gameRetry);
+    }
+
+    private void matchNumbers(Numbers computerNumbers) {
+        while (true) {
+            Numbers playerNumbers = Numbers.create(inputView.readPlayerNumber());
+
+            Hint hint = playerNumbers.generateHint(computerNumbers);
+            if (hint.isThreeStrike()) {
+                outputView.gameEndMessage();
+                break;
+            }
+            outputView.printHintMessage(hint.getHintMessage());
+        }
+    }
+}
