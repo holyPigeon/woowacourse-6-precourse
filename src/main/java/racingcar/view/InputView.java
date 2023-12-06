8fd68c81cc2eed496b25401a14e2f86895c2261e
jae woo Jung <awfjol2008@naver.com>
2023-12-06 22:47:22 +0900

    feat: 경주 시도 횟수를 검증과 함께 입력받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -16,6 +16,9 @@ public class InputView {
         return new InputView();
     }
 
+    /*
+    자동차 이름 목록 입력
+     */
     public List<String> readCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
@@ -46,5 +49,44 @@ public class InputView {
         return Arrays.stream(input.split(","))
                 .map(String::trim)
                 .toList();
+    }
+
+    /*
+    경주 시도 횟수 입력
+     */
+    public Integer readTryCount() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        validateTryCount(input);
+
+        return Integer.parseInt(input);
+    }
+
+    private void validateTryCount(String input) {
+        validateIsBlank(input);
+        validateIsDigit(input);
+    }
+
+    private void validateIsBlank(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException("[ERROR] 입력값이 비어있습니다.");
+        }
+    }
+
+    private void validateIsDigit(String input) {
+        if (!isDigit(input)) {
+            throw new IllegalArgumentException("[ERROR] 입력값이 정수가 아닙니다.");
+        }
+    }
+
+    private boolean isDigit(String input) {
+        try {
+            Integer.parseInt(input);
+
+            return true;
+        } catch (NumberFormatException e) {
+
+            return false;
+        }
     }
 }
