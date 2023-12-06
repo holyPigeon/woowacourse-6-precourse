d693664edb51635578326670ce8a2f51e6e8174f
jae woo Jung <awfjol2008@naver.com>
2023-12-06 20:29:36 +0900

    feat: 자동차 관련 Car 클래스 생성 및 개별 자동차 이름 검증 기능 구현

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,27 @@
+package racingcar.domain;
+
+public class Car {
+
+    private final String name;
+    private Integer distance;
+
+    private Car(String name, Integer distance) {
+        validateName(name);
+        this.name = name;
+        this.distance = distance;
+    }
+
+    public static Car create(String name, Integer distance) {
+        return new Car(name, distance);
+    }
+
+    private void validateName(String name) {
+        validateHasValidLength(name);
+    }
+
+    private void validateHasValidLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 입력값의 길이가 유효하지 않습니다.");
+        }
+    }
+}
