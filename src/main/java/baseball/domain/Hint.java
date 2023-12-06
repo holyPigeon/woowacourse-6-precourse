eed5bb15c87dc8241810bac80d707ca374ac4aa5
jae woo Jung <awfjol2008@naver.com>
2023-12-06 11:04:02 +0900

    feat: 상태에 따른 힌트 메시지 반환 기능 구현

--- src/main/java/baseball/domain/Hint.java
+++ src/main/java/baseball/domain/Hint.java
@@ -14,5 +14,23 @@ public class Hint {
         return new Hint(strike, ball);
     }
 
+    public String getHintMessage() {
+        if (strike == 0 && ball == 0) {
+            return "낫싱";
+        }
 
+        String hintMessage = "";
+
+        if (ball != 0) {
+            hintMessage += ball + "볼 ";
+        }
+        if (strike != 0) {
+            hintMessage += strike + "스트라이크";
+        }
+        return hintMessage;
+    }
+
+    public boolean isThreeStrike() {
+        return strike == 3;
+    }
 }
