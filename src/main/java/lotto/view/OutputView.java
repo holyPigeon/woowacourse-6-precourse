38c628dff9f566814743a8bc5de00781b2ebd593
jae woo Jung <awfjol2008@naver.com>
2023-12-10 01:38:03 +0900

    feat: 수익률 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,7 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,11 +19,18 @@ public class OutputView {
         System.out.println(purchasedLottoCount + "개를 구매했습니다.");
     }
 
-    public void printGeneratedLotto(List<Integer> lotto) {
-        String result = lotto.stream()
-                .map(Object::toString)
+    public void printGeneratedLotto(Lotto lotto) {
+        String result = lotto.getNumbers().stream()
+                .map(lottoNumber -> lottoNumber.getNumber().toString())
                 .collect(Collectors.joining(", ", "[", "]"));
 
         System.out.print(result);
+    }
+
+    public void printWinningResult(String winningResult, double profitRate) {
+        System.out.println("당첨 통계");
+        System.out.println("---");
+        System.out.println(winningResult);
+        System.out.println("총 수익률은 " + profitRate + "%입니다.");
     }
 }
