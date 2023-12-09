88676506ba825a88939c1a20a2f0ae2fef88a73b
jae woo Jung <awfjol2008@naver.com>
2023-12-10 01:48:06 +0900

    feat: Lotto 클래스 생성 파라미터 타입 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,20 +6,24 @@ import java.util.List;
 public class Lotto {
     private final List<LottoNumber> numbers;
 
-    private Lotto(List<LottoNumber> numbers) {
+    private Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = new ArrayList<>(numbers);
+        this.numbers = new ArrayList<>(
+                numbers.stream()
+                        .map(LottoNumber::create)
+                        .toList()
+        );
     }
 
-    public static Lotto create(List<LottoNumber> numbers) {
+    public static Lotto create(List<Integer> numbers) {
         return new Lotto(numbers);
     }
 
-    private void validate(List<LottoNumber> numbers) {
+    private void validate(List<Integer> numbers) {
         validateIsValidLength(numbers);
     }
 
-    private void validateIsValidLength(List<LottoNumber> numbers) {
+    private void validateIsValidLength(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 당첨 번호의 개수가 6개가 아닙니다.");
         }
