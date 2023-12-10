91e9f35dddc742f913665c96543ef471548b4b70
jae woo Jung <awfjol2008@naver.com>
2023-12-10 16:57:04 +0900

    feat: 전체 어플리케이션 동작 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,15 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = InputView.create();
+        OutputView outputView = OutputView.create();
+        LottoController lottoController = LottoController.create(inputView, outputView);
+
+        lottoController.run();
     }
 }
